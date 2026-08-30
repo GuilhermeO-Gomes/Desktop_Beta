04911fd9231c958f9c4c27886cc6e32fca3ae676
GuilhermeO-Gomes <guilherme16062009@gmail.com>
2026-08-30 10:52:47 -0300

    atualizando o banco

--- banco/DesktopBBD.sql
+++ banco/DesktopBBD.sql
@@ -10,4 +10,17 @@ email VARCHAR(100) NOT NULL,
 senha_hash CHAR(64) NOT NULL,
 senha_salt CHAR(32) NOT NULL,
 ativo TINYINT(1) NOT NULL DEFAULT 1
-)
+);
+
+INSERT INTO cliente
+(nome, cpf, data_nascimento, email, senha_hash, senha_salt, ativo)
+VALUES
+(
+    'Cliente Teste',
+    '123.456.789-00',
+    '2000-01-01',
+    'teste@biblioteca.com',
+    SHA2(CONCAT('a1b2c3d4e5f60718293a4b5c6d7e8f90', '123456'), 256),
+    'a1b2c3d4e5f60718293a4b5c6d7e8f90',
+    1
+);
